e64426d067fc2f024233b7a4c9d1de23773d515a
namickey <teethdai@gmail.com>
2016-05-30 00:07:06 +0900

    commit

--- akka-graph/Main.scala
+++ akka-graph/Main.scala
@@ -5,20 +5,47 @@ import akka.stream.scaladsl._
 import akka.util.Timeout
 
 object Main extends App {
-  implicit val system = ActorSystem("system")
-  implicit val mat = ActorMaterializer()
+    Sample1.main()
+    Sample2.main()
+}
 
-  val g = RunnableGraph.fromGraph(GraphDSL.create(){ implicit builder =>
-    import GraphDSL.Implicits._
-    val source = Source(1 to 5)
-    val flow = Flow[Int].map(_ * 2)
-    val sink = Sink.foreach(println)
-    source ~> flow ~> flow ~> sink
-    ClosedShape
-  })
-  g.run()
+object Sample2{
+  def main() = {
+    implicit val system = ActorSystem("system")
+    implicit val mat = ActorMaterializer()
+
+    val g = RunnableGraph.fromGraph(GraphDSL.create(){ implicit builder =>
+      import GraphDSL.Implicits._
+      val source = Source(1 to 5)
+      val flow = Flow[Int].map(_ * 3)
+      val sink = Sink.foreach(println)
+      source ~> flow ~> flow ~> sink
+      ClosedShape
+    })
+    g.run()
   
-  Thread.sleep(2000)
-  system.shutdown()
+    Thread.sleep(2000)
+    system.shutdown()
+  }
+}
+
+
+object Sample1{
+  def main() = {
+    implicit val system = ActorSystem("system")
+    implicit val mat = ActorMaterializer()
+
+    val g = RunnableGraph.fromGraph(GraphDSL.create(){ implicit builder =>
+      import GraphDSL.Implicits._
+      val source = Source(1 to 5)
+      val flow = Flow[Int].map(_ * 2)
+      val sink = Sink.foreach(println)
+      source ~> flow ~> flow ~> sink
+      ClosedShape
+    })
+    g.run()
   
+    Thread.sleep(2000)
+    system.shutdown()
+  }
 }
